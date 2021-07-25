09027327637a06f63718b85ccfce0e8cba5ff391
yohei0125 <yohei0125@weseek.co.jp>
2021-07-26 01:42:30 +0900

    create first page

---
+++ tailwind.config.js
@@ -0,0 +1,45 @@
+module.exports = {
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    letterSpacing: {
+      widest: ".3em",
+    },
+    container: {
+      center: true,
+    },
+    extend: {
+      height: {
+        200: "200px",
+        250: "250px",
+        300: "300px",
+        350: "350px",
+        400: "400px",
+        450: "450px",
+        500: "500px",
+        550: "550px",
+      },
+      spacing: {
+        21: "10rem",
+      },
+      zIndex: {
+        "-10": "-10",
+        "-20": "-20",
+      },
+      inset: {
+        "1/5": "20%",
+        "15%": "15%",
+        "10%": "10%",
+        "5%": "5%",
+      },
+      backgroundImage: (theme) => ({
+        myself: "url('/img_myself.svg')",
+        engineering: "url('/img_engineering.svg')",
+      }),
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [require("@tailwindcss/typography")],
+};
